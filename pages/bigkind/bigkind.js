e778f1e3607fe1091f8e3bb48bf2e3fc36e580b6
nireqing <nireqing@163.com>
2018-11-09 16:20:20 +0800

    add:bigkind

--- pages/bigkind/bigkind.js
+++ pages/bigkind/bigkind.js
@@ -6,30 +6,53 @@ Page({
     kindcontent: [],
     kindbanner: []
   },
+  // 跳转到详情页
+  todetail(e) {
+    wx.navigateTo({
+      url: `/pages/detail/detail?id=${e.currentTarget.dataset.id}`,
+    })
+  },
+  //点击改变下面数据
+  getkind(e){
+    this.getdata(e.currentTarget.dataset.id + 2)
+  },
+  //获取该分类下的商品
+    getdata(id){
+      // 请求id2430
+      ajax.get(`https://api.imeihao.shop/recept/common/homepage/${id}/modules`)
+        .then(resp => {
+          const bigid = resp.data.data[1].details.id
+          //请求一下分类idKF20171224185350DVJZKV
+          ajax.get(`https://api.imeihao.shop/recept/common/activity/v2/module/${bigid}/containers?offset=0&limit=6`)
+            .then(resp => {
+              const littleid = resp.data.data.containers[0].kind_id
+              //请求分类下的所有商品
+              ajax.get(`https://api.imeihao.shop/recept/common/kind/${littleid}/products/paging?limit=6&offset=0`)
+                .then(resp => {
+                  this.setData({
+                    kindcontent: resp.data.data
+                  })
+                })
+            })
+      })
+      //请求banner
+      ajax.get(
+        `https://api.imeihao.shop/recept/common/homepage/${id}/modules/paging?limit=6&offset=0`)
+        .then(resp => {
+          // console.log(resp.data.data.modules[0].details.containers[0].pic)
+          this.setData({
+            kindbanner: resp.data.data.modules[0].details.containers[0].pic
+          })
+      })
+    },
   onLoad: function (options) {
+    this.getdata(options.id)
     // 请求导航的分类
     ajax.get(
       'https://api.imeihao.shop/recept/common/homepage/navigations')
       .then(resp => {
         this.setData({
           homenav: resp.data.data
-        })
-    }),
-    // 请求分类下面的数据
-    ajax.get(
-      'https://api.imeihao.shop/recept/common/kind/KF20171224185350ZUCWIX/products/paging?limit=6&offset=0')
-      .then(resp => {
-        this.setData({
-          kindcontent: resp.data.data
-        })
-    }),
-    //请求banner
-    ajax.get(
-      'https://api.imeihao.shop/recept/common/homepage/3/modules/paging?limit=6&offset=0')
-      .then(resp => {
-        // console.log(resp.data.data.modules[0].details.containers[0].pic)
-        this.setData({
-          kindbanner: resp.data.data.modules[0].details.containers[0].pic
         })
     })
   }
--- pages/home/home.js
+++ pages/home/home.js
@@ -24,84 +24,83 @@ Page({
       url: `/pages/detail/detail?id=${e.currentTarget.dataset.id}`,
     })
   },
-  tobigkind() {
+  tobigkind(e) {
     wx.navigateTo({
-      url: '/pages/bigkind/bigkind',
+      url: `/pages/bigkind/bigkind?id=${e.currentTarget.dataset.id+2}`,
     })
   },
-  /**
- * 生命周期函数--监听页面加载
- */
+
   onLoad: function (options) {
-  // 请求导航的分类
-    ajax.get(
-      'https://api.imeihao.shop/recept/common/homepage/navigations')
-      .then(resp => {
-        this.setData({
-          homenav: resp.data.data
+    console.log()
+    // 请求导航的分类
+      ajax.get(
+        'https://api.imeihao.shop/recept/common/homepage/navigations')
+        .then(resp => {
+          this.setData({
+            homenav: resp.data.data
+          })
+      }),
+    // post测试
+      ajax.post(
+        'https://api.imeihao.shop/recept/common/search',
+        {
+          keyword: "项链",
+          offset: 8,
+          size: 8,
+          type: "product"
         })
-    }),
-  // post测试
-    ajax.post(
-      'https://api.imeihao.shop/recept/common/search',
-      {
-        keyword: "项链",
-        offset: 8,
-        size: 8,
-        type: "product"
+        .then(resp => {
+          // console.log(resp)
+        })
+    // 请求轮播图
+      ajax.get(
+        'https://api.imeihao.shop/recept/common/homepage/2/modules/paging?limit=6&offset=0')
+        .then(resp => {
+          this.setData({
+            imgUrls: resp.data.data.modules[0].details.containers
+          })
       })
-      .then(resp => {
-        // console.log(resp)
+    // 请求home的四大分类
+      ajax.get(
+        'https://api.imeihao.shop/recept/common/homepage/2/modules/paging?limit=6&offset=0')
+        .then(resp => {
+          this.setData({
+            imgfour: resp.data.data.modules[2].details.containers
+          })
       })
-  // 请求轮播图
-    ajax.get(
-      'https://api.imeihao.shop/recept/common/homepage/2/modules/paging?limit=6&offset=0')
-      .then(resp => {
-        this.setData({
-          imgUrls: resp.data.data.modules[0].details.containers
-        })
-    })
-  // 请求home的四大分类
-    ajax.get(
-      'https://api.imeihao.shop/recept/common/homepage/2/modules/paging?limit=6&offset=0')
-      .then(resp => {
-        this.setData({
-          imgfour: resp.data.data.modules[2].details.containers
-        })
-    })
-  // home的四张图
-    ajax.get(
-      'https://api.imeihao.shop/recept/common/homepage/2/modules/paging?limit=6&offset=0')
-      .then(resp => {
-        this.setData({
-          homefour: resp.data.data.modules[1].details.containers
-        })
-    })
-  // 新品上架的标题图
-    ajax.get(
-      'https://api.imeihao.shop/recept/common/homepage/2/modules/paging?limit=6&offset=0')
-      .then(resp => {
-        this.setData({
-          homenewtitle: resp.data.data.modules[3].details.containers[0]
-        })
-    })
-  // 新品上架的内容
-    ajax.get(
-      'https://api.imeihao.shop/recept/common/homepage/2/modules/paging?limit=6&offset=0')
-      .then(resp => {
-        this.setData({
-          homenewthings: resp.data.data.modules[4].details.containers
-        })
-    })
-  //美食控的头部
-    ajax.get(
-      'https://api.imeihao.shop/recept/common/homepage/2/modules/paging?limit=6&offset=6')
-      .then(resp => {
-        this.setData({
-          msktitle: resp.data.data.modules[3].details.containers[0]
-        })
-    })
-  //美食控的内容
+    // home的四张图
+      ajax.get(
+        'https://api.imeihao.shop/recept/common/homepage/2/modules/paging?limit=6&offset=0')
+        .then(resp => {
+          this.setData({
+            homefour: resp.data.data.modules[1].details.containers
+          })
+      })
+    // 新品上架的标题图
+      ajax.get(
+        'https://api.imeihao.shop/recept/common/homepage/2/modules/paging?limit=6&offset=0')
+        .then(resp => {
+          this.setData({
+            homenewtitle: resp.data.data.modules[3].details.containers[0]
+          })
+      })
+    // 新品上架的内容
+      ajax.get(
+        'https://api.imeihao.shop/recept/common/homepage/2/modules/paging?limit=6&offset=0')
+        .then(resp => {
+          this.setData({
+            homenewthings: resp.data.data.modules[4].details.containers
+          })
+      })
+    //美食控的头部
+      ajax.get(
+        'https://api.imeihao.shop/recept/common/homepage/2/modules/paging?limit=6&offset=6')
+        .then(resp => {
+          this.setData({
+            msktitle: resp.data.data.modules[3].details.containers[0]
+          })
+      })
+    //美食控的内容
     ajax.get(
       'https://api.imeihao.shop/recept/common/homepage/2/modules/paging?limit=6&offset=6')
       .then(resp => {  
@@ -110,16 +109,8 @@ Page({
           msk: resp.data.data.modules[4].details.containers
         })
     })
-    //下拉加载更多
-    // ajax.get(
-    //   'https://api.imeihao.shop/recept/common/meihao+/display')
-    //   .then(resp => {
-    //     console.log(resp)
-    //   })
   },
-  /**
-   * 生命周期函数--监听页面显示
-   */
+
   onShow: function () {
     app.setBadge()
   },
